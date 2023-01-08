7fbd6d61aadcdc8eb9b19a4bbfea15bb63956d76
Miguel Casique <migueljosecasique@gmail.com>
2023-01-08 01:45:04 -0400

    Prep 2 finished, conditionals

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      Console.Write("What was your course grade percentage? ");
+      String courseGrade= Console.ReadLine();
+      int coursePercent = int.Parse(courseGrade);
+
+      string grade= "";
+
+      if (coursePercent >= 90)
+      {
+          grade= "A";
+      }
+      else if (coursePercent >= 80)
+      {
+          grade = "B";
+      }
+      else if (coursePercent >= 70)
+      {
+          grade = "C";
+      }
+      else if (coursePercent >= 60)
+      {
+          grade = "D";
+      }
+      else
+      {
+          grade = "F";
+      }
+      Console.WriteLine($"Your final grade is: {grade}");
+      if (coursePercent >= 70)
+      {
+          Console.WriteLine("Congratulations, you passed the course!!!");
+      }
+      else
+      {
+          Console.WriteLine("You can do it better next time!!!");
+      }    
     }
 }
