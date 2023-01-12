274440e73dcfd60c4326696f5bc1f580280d6998
Miguel Casique <migueljosecasique@gmail.com>
2023-01-12 01:48:38 -0400

    Prep 3 assigment completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,46 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber;
+        String userGuess;
+        int iUserGuess;
+        String input = "yes";
+        while (input == "yes")
+        {   iUserGuess = 0;
+            magicNumber = randomGenerator.Next(1, 101);
+            //prompting the user for the game information.
+            while (iUserGuess != magicNumber)
+            {           
+                Console.Write("What is your guess? ");
+                userGuess = Console.ReadLine();
+                iUserGuess = int.Parse(userGuess);
+
+                //code that makes the game works.
+                if (iUserGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (iUserGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else 
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            
+            }
+        Console.Write("Do you want to continue (yes/no)? ");
+        string newInput = Console.ReadLine();
+        if (newInput == "no")
+        {
+            input= newInput;
+        }
+        else
+        {
+            input = "yes";
+        }
+        }           
     }
 }
