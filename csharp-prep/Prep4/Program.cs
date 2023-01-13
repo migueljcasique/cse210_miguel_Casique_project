ea1c43fd63c73cdaa20a98bbb77927eed3d53470
Miguel Casique <migueljosecasique@gmail.com>
2023-01-13 00:59:50 -0400

    Prep4 Completed, list assigment done.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        String sInput;
+        int userInput;
+        Console.WriteLine("Enter a list of numbers,type 0 when finished.");
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            sInput= Console.ReadLine();
+            userInput= int.Parse(sInput);
+            if (userInput == 0)
+            {
+                break;
+            }
+            numbers.Add(userInput);
+        }
+        int listSum = numbers.Sum();
+        Console.WriteLine($"The sum is : {listSum}");
+        int listTotal= numbers.Count();
+        int listAvg = listSum/listTotal;
+        Console.WriteLine($"The average is: {listAvg}");
+        int largestValue= numbers.Max();
+        Console.WriteLine($"The largest number is: {largestValue}");
     }
 }
