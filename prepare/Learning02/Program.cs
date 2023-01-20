1fe2b3ea39910c5cce06e527cf5f352cc1b9ccb6
Miguel Casique <migueljosecasique@gmail.com>
2023-01-20 01:03:59 -0400

    learning activity done

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+    Job job1 = new Job();
+    Job job2 = new Job();
+    job1._jobTitle= "Software Engineer";
+    job1._company = "Microsoft";
+    job1._startYear= 2019;
+    job1._endYear = 2022;
+    job2._jobTitle = "Manager";
+    job2._company = "Apple";
+    job2._startYear= 2022;
+    job2._endYear = 2023;
+    Resume userResume = new Resume();
+    userResume._name = "Allison Robe"; 
+    userResume._jobs.Add(job1);
+    userResume._jobs.Add(job2);
+    userResume.DisplayResume(); 
+     
     }
 }
